35c8012885811e1995bcdce12096c959629fdbca
Dani <dpajaresfo@gmail.com>
2025-04-14 20:01:42 +0200

    mejora de adivina el numero.

--- js/adivina.js
+++ js/adivina.js
@@ -1,15 +1,17 @@
 function adivinaNumero(){
     const num = Math.floor(Math.random()*10+1);
     let intento;
+    let contador = 1;
 
     do{
         intento = parseInt(prompt("Adivina el numero (entre1 y 10):"));
+        contador++;
 
         if(isNaN(intento)){
             alert("Por favor, escribe un numero válido.");
         }else if (intento !== num){
             alert("¡Incorrecto!, intenta de nuevo.")
-
+            document.getElementById("resultado").innerHTML = '¡Adivinastes el numero es: <strong>' + contador + ' </strong> intento(s)!';
         }
     }while(intento!==num);
 
